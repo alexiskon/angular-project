690f1d8655360b99a4997edabcc343e3abe1bd89
tolisdotkarag <aposkarag@gmail.com>
2020-12-04 12:55:50 +0200

    User story 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserStory1Module } from './user-story-1/user-story-1.module';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { GetDataComponent } from './user-story-1/get-data/get-data.component';
+import { BugFormComponent } from './user-story-2/bug-form/bug-form.component';
+import { UserStory2Module } from './user-story-2/user-story-2.module';
+
+const routes: Routes = [
+{
+  path:'',  component: GetDataComponent
+},
+{
+  path:'bug_reporting_form', component: BugFormComponent
+}
+]
+
 
 @NgModule({
   declarations: [
@@ -13,7 +27,9 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     UserStory1Module,
-    HttpClientModule
+    HttpClientModule,
+    UserStory2Module,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]

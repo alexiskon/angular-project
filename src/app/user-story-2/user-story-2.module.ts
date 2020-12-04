5bb6bbe74aeea208b2be7491ca05640681e9bf80
Alexandros Kontos <alexiskon01@gmail.com>
2020-12-04 12:55:29 +0200

    ust1 final

---
+++ src/app/user-story-2/user-story-2.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BugFormComponent } from './bug-form/bug-form.component';
+
+
+
+@NgModule({
+  declarations: [BugFormComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [BugFormComponent]
+})
+export class UserStory2Module { }

ad963b2767e302b345c3390565e11cb7ef46b7f8
tolisdotkarag <aposkarag@gmail.com>
2020-12-04 11:51:51 +0200

    services folder

--- src/app/user-story-1/get-data/get-data.component.ts
+++ src/app/user-story-1/get-data/get-data.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Bugs } from '../../interfaces/bugs'
-import { Ust1Service } from '../../services/ust1.service';
+import { Ust1Service } from '../../services/ust1-services/ust1.service';
 import { faSortAlphaDown } from '@fortawesome/free-solid-svg-icons';
 import { faSortNumericDown } from '@fortawesome/free-solid-svg-icons';
 import { faSortAlphaUp } from '@fortawesome/free-solid-svg-icons';
 import { faSortNumericUp } from '@fortawesome/free-solid-svg-icons';
 import { faSort } from '@fortawesome/free-solid-svg-icons';
-import { GetSortedDataService } from 'src/app/services/get-sorted-data.service';
+import { GetSortedDataService } from 'src/app/services/ust1-services/get-sorted-data.service';
 
 
 @Component({
@@ -29,7 +29,7 @@ export class GetDataComponent implements OnInit {
   bugs: Bugs[] = [];
   // sortDesc[i] is true when the i-th header is sorted descendingly
   // [0:title, 1:priority, 2:reporter, 3:createdAt, 4:status]
-  sortDesc: boolean[]= [false,false,false,false,false];
+  sortDesc: boolean[]= [false,true,false,true,false];
   constructor(private ust1: Ust1Service, private getSortedService: GetSortedDataService) { }
 
 
@@ -41,25 +41,6 @@ export class GetDataComponent implements OnInit {
         this.bugs.push(it)
       })
     })
-
-    // console.log(this.bugs);
-
-    //Second way to get bugs from API
-    // this.ust1.getBugs().subscribe((data) => {
-    //   this.bugs = data.map(it => {
-    //     return {
-    //       id: it.id,
-    //       title: it.title,
-    //       description: it.description,
-    //       priority: it.priority,
-    //       reporter: it.reporter,
-    //       status: it.status,
-    //       updatedAt: it.updatedAt,
-    //       createdAt: it.createdAt,
-    //       comments: it.comments
-    //     };
-    //   })
-    // })
   }
 
   getHeader(event: Event): void {
@@ -68,6 +49,8 @@ export class GetDataComponent implements OnInit {
 
     // According to the id we send a request to the API and we get the sorted
     // data from url?sort=${id},${order} where order is by default ascending
+    // for alphabetical values, descending for priority to show the most 
+    // important first and date from newest to oldest
     // except for the time when we click multiple times the header so in this 
     // case we inverse the order. If another header is clicked, then the order  
     // of the other tabs are changed back to ascending.
@@ -80,7 +63,7 @@ export class GetDataComponent implements OnInit {
           this.bugs.push(it)
         })
       })
-      this.sortDesc = [!this.sortDesc[0],false,false,false,false];
+      this.sortDesc = [!this.sortDesc[0],true,false,true,false];
     } else if (value == 'priority') {
       this.counters = [0, 1, 0, 0, 0];
       this.bugs = [];
@@ -89,7 +72,7 @@ export class GetDataComponent implements OnInit {
           this.bugs.push(it)
         })
       })
-      this.sortDesc = [false,!this.sortDesc[1],false,false,false];
+      this.sortDesc = [false,!this.sortDesc[1],false,true,false];
 
     } else if (value == 'reporter') {
       this.counters = [0, 0, 1, 0, 0];
@@ -99,7 +82,7 @@ export class GetDataComponent implements OnInit {
           this.bugs.push(it)
         })
       })
-      this.sortDesc = [false,false,!this.sortDesc[2],false,false];
+      this.sortDesc = [false,true,!this.sortDesc[2],true,false];
 
     } else if (value == 'createdAt') {
       this.counters = [0, 0, 0, 1, 0];
@@ -109,7 +92,7 @@ export class GetDataComponent implements OnInit {
           this.bugs.push(it)
         })
       })
-      this.sortDesc = [false,false,false,!this.sortDesc[3],false];
+      this.sortDesc = [false,true,false,!this.sortDesc[3],false];
 
     } else if (value == 'status') {
       this.counters = [0, 0, 0, 0, 1];
@@ -119,7 +102,7 @@ export class GetDataComponent implements OnInit {
           this.bugs.push(it)
         })
       })
-      this.sortDesc = [false,false,false,false,!this.sortDesc[4]];
+      this.sortDesc = [false,true,false,true,!this.sortDesc[4]];
     }
   }
 }

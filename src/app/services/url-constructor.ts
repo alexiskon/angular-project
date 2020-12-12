a11c70b4d78863ee9a66fd22145de1beb087215b
Alexandros Kontos <alexiskon01@gmail.com>
2020-12-12 16:13:38 +0200

    sorting fix

--- src/app/services/url-constructor.ts
+++ src/app/services/url-constructor.ts
@@ -14,16 +14,7 @@ export class UrlConstructor {
   urlResults (pageNum: number, pageSize: number, desc?: boolean, header?: string, title?: string | "", priority?: string | "", reporter?: string | "", status?: string | ""): Observable<Bugs[]> {
     let sortOrder = desc  ? 'desc': 'asc'; 
     let headerOrder = header === "" ? "": header + ',' + sortOrder;
-    //checkfor undefined
-    let checkForUndefined = [title, priority, status, reporter];
-    for (let item of checkForUndefined) {
-      if (item === undefined) {
-        item = "";
-      }
-    }
-
-    console.log(headerOrder, title, priority)
-
+ 
     return this.http.get<Bugs[]>(`${this.url}?page=${pageNum}&size=${pageSize}
     &title=${title}&priority=${priority}&reporter=${reporter}&status=${status}&sort=${headerOrder}`)
   }

aaa31601e88efa1a6a8b6b5eea9ce9ee3091a917
Alexandros Kontos <alexiskon01@gmail.com>
2020-12-19 13:52:38 +0200

    fdescribe

--- src/app/create-edit-form/bug-form/bug-form.component.spec.ts
+++ src/app/create-edit-form/bug-form/bug-form.component.spec.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from 'src/app/app-routing.module';
 
 import { BugFormComponent } from './bug-form.component';
 
-describe('BugFormComponent', () => {
+fdescribe('BugFormComponent', () => {
   let component: BugFormComponent;
   let fixture: ComponentFixture<BugFormComponent>;
 
@@ -21,27 +21,27 @@ describe('BugFormComponent', () => {
     component = fixture.componentInstance;
   });
 
-  fit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
   //9-a
-  fit('form should be invalid if there are no input data', () => {
+  it('form should be invalid if there are no input data', () => {
     component.initializeForm(null, null, null, null, null);
     expect(component.bugForm.invalid).toBeTruthy();
   })
   //9-b
-  fit('the form should be valid by having the minimum required fields', () => {
+  it('the form should be valid by having the minimum required fields', () => {
     component.initializeForm('title', 'description', '1', 'PO', null)
     expect(component.bugForm.valid).toBeTruthy();
   })
   //9-c
-  fit('form should be invalid if reporter is QA', () => {
+  it('form should be invalid if reporter is QA', () => {
     component.initializeForm('title', 'description', '1', "QA", null)
     fixture.detectChanges();
     expect(component.bugForm.invalid).toBeTruthy();
   })
   //9-d
-  fit('form should be valid if reporter is QA', () => {
+  it('form should be valid if reporter is QA', () => {
     component.initializeForm('title', 'description', '1', 'QA', 'Done')
     expect(component.bugForm.valid).toBeTruthy();
   })
